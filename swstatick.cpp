fa43812b162ee8c697b1ed5451a72b541ce7ce39
Madhav <2021ec0473@svce.ac.in>
2024-07-03 22:07:17 +0530

    swstatick

---
+++ swstatick.cpp
@@ -0,0 +1,14 @@
+int swstatic(vector<int>& nums, int k) {
+    int curr = 0;
+    for (int i = 0; i < k; i++) {
+        curr += nums[i];
+    }
+    
+    int ans = curr;
+    for (int i = k; i < nums.size(); i++) {
+        curr += nums[i] - nums[i - k];
+        ans = max(ans, curr);
+    }
+    
+    return ans;
+}
